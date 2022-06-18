8cd4d80e676e76e4a89a6ff7db23bd3e286dd81a
cshober <44792851+cshober@users.noreply.github.com>
2022-06-17 20:15:18 -0400

    initial commit (finally

--- projects/budgetApp/budget.py
+++ projects/budgetApp/budget.py
@@ -5,7 +5,6 @@ class Category:
         self.ledger = []
         self.amount = 0.0
         self.name = name
-        print('weiner')
 
     def __str__(self):
         title = f"{self.name:*^30}\n"
